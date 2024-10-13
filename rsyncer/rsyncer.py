99eb7b038aebbc668dfc4337386c8bd415548f93
Doug Keller <dg.kllr.jr@gmail.com>
2024-10-13 23:00:55 +0200

    added run force option

--- rsyncer/rsyncer.py
+++ rsyncer/rsyncer.py
@@ -14,6 +14,34 @@ rsyncer_version = importlib.metadata.version('rsyncer')
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 #  ──────────────────────────────────────────────────────────────────────────
+# global functions
+
+def _log(config, content):
+    with open(Path(config['log']['path']).expanduser(), 'a') as file:
+        file.write(content + '\n')
+
+
+def _run_freq(config, source, destination, frequency):
+    cmd = ['rsync', '--progress', '-r', source, destination + '/' + frequency]
+    subprocess.run(cmd)
+
+
+def _run(config, frequency):
+    for source in config['sources']:
+        for path in config['sources'][source]['paths']: # for each source's path
+            for destination in config['destinations']: # for each destination
+
+                if source in ['local']:
+                    _run_freq( config, Path(path).expanduser(), destination + ':' + config['destinations'][destination]['path'] , frequency )
+                else:
+                    _run_freq( config, source + ':' + str(Path(path).expanduser()), destination + ':' + config['destinations'][destination]['path'], frequency )
+
+#  ──────────────────────────────────────────────────────────────────────────
+# global options
+
+force_option = click.option('--force', is_flag=True, default=False, show_default=True, help='Force command', type=bool)
+
+#  ──────────────────────────────────────────────────────────────────────────
 # base command
 
 # done for the config
@@ -55,31 +83,28 @@ def rsyncer(ctx, config):
 # run command
 
 @rsyncer.command('run', short_help="Run rsyncer.")
+@click.option('-s', '--sleep', 'loop_sleep', default=3, help="Sleep time in hours within run loop.", type=int)
+@force_option
 @click.pass_obj
-def run(obj):
+def run(obj, loop_sleep, force):
     """Run"""
 
-    def _log(config, content):
-        with open(Path(config['log']['path']).expanduser(), 'a') as file:
-            file.write(content + '\n')
+    def _run_loop(config, current_time, old_time, force):
+        for frequency in config['frequency']['frequencies']:
+            if frequency == 'daily':
+                if current_time.day > old_time.day or force:
+                    _run(config, frequency)
+                    _log(config, current_time.isoformat() + ' ' + frequency)
 
+            elif frequency == 'weekly':
+                if current_time.weekday() == 6 or force: 
+                    _run(config, frequency)
+                    _log(config, current_time.isoformat() + ' ' + frequency)
 
-    def _run_freq(config, source, destination, frequency):
-        # cmd = ['echo', source, destination + '/' + frequency]
-        cmd = ['rsync', '--progress', '-r', source, destination + '/' + frequency]
-        subprocess.run(cmd)
-
-
-    def _run(config, frequency):
-
-        for source in config['sources']:
-            for path in config['sources'][source]['paths']: # for each source's path
-                for destination in config['destinations']: # for each destination
-
-                    if source in ['local']:
-                        _run_freq( config, Path(path).expanduser(), destination + ':' + config['destinations'][destination]['path'] , frequency )
-                    else:
-                        _run_freq( config, source + ':' + str(Path(path).expanduser()), destination + ':' + config['destinations'][destination]['path'], frequency )
+            elif frequency == 'monthly':
+                if current_time.day == 1 or force: 
+                    _run(config, frequency)
+                    _log(config, current_time.isoformat() + ' ' + frequency)
 
 
     # run time loop
@@ -87,24 +112,11 @@ def run(obj):
     while True:
         current_time = datetime.now()
 
-        for frequency in obj['frequency']['frequencies']:
+        if current_time > old_time or force:
+            _run_loop(obj, current_time, old_time, force)
 
-            if current_time > old_time:
-
-                if frequency == 'daily':
-                    if current_time.day > old_time.day:
-                        _run(obj, frequency)
-                        _log(obj, current_time.isoformat() + ' ' + frequency)
-
-                elif frequency == 'weekly':
-                    if current_time.weekday() == 6: 
-                        _run(obj, frequency)
-                        _log(obj, current_time.isoformat() + ' ' + frequency)
-
-                elif frequency == 'monthly':
-                    if current_time.day == 1: 
-                        _run(obj, frequency)
-                        _log(obj, current_time.isoformat() + ' ' + frequency)
+            if force:
+                return
 
         old_time = current_time
-        sleep(3*3600) # waits for 3 hours
+        sleep(loop_sleep*3600) # waits for 3 hours
--- scripts/build_push_pypi.sh
+++ scripts/build_push_pypi.sh
@@ -5,7 +5,7 @@ python3 -m pip install --upgrade build twine
 cd ..
 echo `pwd`
 python3 -m build
-python3 -m twine upload --repository testpypi --verbose dist/*
-#python3 -m twine upload --verbose dist/*
-rm -r douglog.egg-info
+#python3 -m twine upload --repository testpypi --verbose dist/*
+python3 -m twine upload --verbose dist/*
+rm -r rsyncer.egg-info
 rm -r dist
